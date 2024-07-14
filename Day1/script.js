bc10df10e4a8622047ab2e471f15a10e2488d081
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-07-14 17:24:11 +0530

    Day 1

---
+++ Day1/script.js
@@ -0,0 +1,69 @@
+// Activity 1: Variable Declaration
+// Task 1
+var a = 10;
+console.log(a)
+
+// Task 2
+let str = "Mohit"
+console.log(str)
+
+// Activity 2: Constant Declaration
+// Task 3
+const boo = false
+console.log(boo);
+
+// Activity 3: Data Types
+// Task 4
+var num = 10;
+console.log(typeof num);
+
+let str1 = "Ruchita";
+console.log(typeof str1);
+
+let booValues = true;
+console.log(typeof booValues);
+
+let arr = [1,2,3,4,5];
+console.log(typeof arr);
+
+let obj = {
+    name:"Ruchita",
+    type:"In-Love"
+}
+console.log(typeof obj)
+
+// Activity 4: Reassigning Variables
+// Task 5
+
+let initial = 10;
+console.log(initial)
+initial = 20;
+console.log(initial)
+
+// Activity 5: Understanding const 
+// Task 6
+// const abc  = 10;
+// console.log(abc)
+// abc = 20;
+// console.log(abc)
+// script.js:47 Uncaught TypeError: Assignment to constant variable.
+
+// Feature Request
+// 1.
+var aFeature = 10;
+console.log(aFeature);
+console.log(typeof aFeature)
+
+let strFeature = "Ruchita"
+console.log(strFeature);
+console.log(typeof strFeature)
+
+let booFeature = true;
+console.log(booFeature);
+console.log(typeof booFeature);
+
+
+
+
+
+
